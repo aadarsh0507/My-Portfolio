0f8495ba8b004ddad100120bd0f464a0740f624e
aadarsh0507 <infor0507@gmail.com>
2025-06-17 13:36:45 +0530

    removed a unwamted spaces

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,9 +3,9 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 overflow-x-hidden">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow w-full">
         {children}
       </main>
       <Footer />
